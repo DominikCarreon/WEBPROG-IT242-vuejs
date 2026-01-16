747bb74d42e8a4ed02b6edfc5add207edce2d712
DominikCarreon <dvcarreon@student.apc.edu.ph>
2026-01-16 11:27:29 +0800

    fix

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,69 +1,58 @@
 <template>
-
-<body>
- 
-<h1>Personal Profile Web page </h1>
-
- <p>
-About Me<br>
-Education / Achievements<br>
-Senior high school at Asia pacific college<br> 
-  
-Course <br>
-BS Information technology <br>
-  
-IT Experience<br>
-HTML/CSS/JAVA/PYTHON<br>
-  
-Hobbies & Interests<br>
-Eating and drinking lots of coffee  
-  
-Goals In Life / Dream<br>
-Goal in life is to be Happy and Rich
-  
-Picture Gallery<br>
-</p>
-<img src="https://as-prod.asyncgw.teams.microsoft.com/v1/objects/0-ea-d7-f6f35dd33910abd4d61fb0dd986223cd/views/imgpsh_fullsize" alt="Girl with a jacket" width="500" height="600">
- 
-
-
+<div class="profile-page">
+<h1>Personal Profile Web page</h1>
+<section>
+<h2>About Me</h2>
+<p>Education / Achievements</p>
+<p>Senior high school at Asia Pacific College</p>
+<p><strong>Course:</strong> BS Information Technology</p>
+<p><strong>IT Experience:</strong> HTML / CSS / Java / Python</p>
+<p><strong>Hobbies & Interests:</strong> Eating and drinking lots of coffee</p>
+<p><strong>Goals In Life / Dream:</strong> To be happy and rich</p>
+</section>
+<section>
+<h2>Picture Gallery</h2>
+<img
+       src="https://as-prod.asyncgw.teams.microsoft.com/v1/objects/0-ea-d7-f6f35dd33910abd4d61fb0dd986223cd/views/imgpsh_fullsize"
+       alt="Profile picture"
+       width="500"
+       height="600"
+     />
+</section>
 <div class="grid-container">
-
-<div class="header"><h1>Dominik's Personal Profile </h1></div>
-
+<div class="header">
+<h1>Dominik's Personal Profile</h1>
+</div>
 <div class="menu">
-  <ul>
-    <li> Linkedin  </li>
-    <li> Facebook  </li>
-    <li> Instagram </li>
-  </ul>
+<ul>
+<li>LinkedIn</li>
+<li>Facebook</li>
+<li>Instagram</li>
+</ul>
 </div>
-
 <div class="content">
-  <h1>  About me </h1>
-  <p>Dominik's Nickname is Doms</p>
-  <p>Dom's is a good student he can finish all requierments on time</p>
-  <p>Dom's is a very happy person and sometimes very annoying</p>
-  <p>Dom's Like Coffee Really bad. </p>
-</div>  
-
-<div class="facts">
-  <h2>Facts:</h2>
-  <ul>
-    <li>I like reading books</li>
-    <li>I can study 24 hours straight</li>
-  </ul>
+<h1>About Me</h1>
+<p>Dominik's nickname is Doms.</p>
+<p>Doms is a good student and finishes all requirements on time.</p>
+<p>Doms is a very happy person and sometimes very annoying.</p>
+<p>Doms really likes coffee.</p>
 </div>
-
-<div class="footer"><p>© 2025 Dominik's Profile, Inc. All rights reserved.</p></div>
-
-</body>
+<div class="facts">
+<h2>Facts:</h2>
+<ul>
+<li>I like reading books</li>
+<li>I can study 24 hours straight</li>
+</ul>
+</div>
+<div class="footer">
+<p>© 2025 Dominik's Profile, Inc. All rights reserved.</p>
+</div>
+</div>
+</div>
 </template>
-
-
 <script>
-
+export default {
+ name: "PersonalProfile",
+};
 </script>
-
 <style src="./css/personalprofile.css"></style>
-  
