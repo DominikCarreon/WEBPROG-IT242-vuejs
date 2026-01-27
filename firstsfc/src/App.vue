b1171373d267abf3d232af04df8ea554234d07ea
DominikCarreon <dvcarreon@student.apc.edu.ph>
2026-01-27 09:45:11 +0800

    fix

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,21 @@
-<template>
-  <div>
-    <personal-profile />
-  </div>
-</template>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
 
-<script>
-export default {
-  name: "App"
+const instruments = ref([])
+
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
 }
+
+onMounted(() => {
+   getInstruments()
+})
 </script>
+
+<template>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
+</template>
